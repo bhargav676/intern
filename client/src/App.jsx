3fa548fd5f2da7b152975d722946c0a22729e022
Shivaram <323103311053@gvpce.ac.in>
2025-05-29 11:44:36 +0530

    first

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 className=' bg-amber-200'>hello</h1>
+      <h1 className=' bg-amber-200'>hello
+        world
+      </h1>
     </div>
   )
 }
